3de09932e1cee5280f0d09d932101ff416e91fda
cameliabenavides10 <cameliabenavides@gmail.com>
2023-03-20 15:55:32 -0500

    starter code

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb+srv://cameliabenavides10:Camels123!@cluster0.th1ulwz.mongodb.net/programming-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
