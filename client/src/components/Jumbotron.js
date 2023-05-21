aa5471920e592d936bdd82fb65cd156620c3d3d3
cameliabenavides10 <cameliabenavides@gmail.com>
2023-05-21 12:43:15 -0500

    some styling to the home page

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { QUERY_MEALPLAN, QUERY_ME } from '../utils/queries';
+import groceries from '../assets/groceries.png'
 
 
 
@@ -18,31 +19,37 @@ return(
   <p class="lead">Welcome to Meal Planner Pro</p>
   <hr class="my-3" /> */}
    <div class="container">
-        <div class="row">
+        <div class="row ">
           <div class="col-sm-6 "> 
-  <p class="display-5" >The ultimate solution for anyone looking to plan their meals in a hassle-free way while keeping their dietary restrictions in mind. Our website is designed to help you create a personalized meal plan for the week that perfectly suits your lifestyle and dietary preferences.</p>
+          <h1 className="display-2">
+            GutGuide
+          </h1>
+  <p class="display-5 d-flex justify-content-center" >The ultimate solution for anyone looking to plan their meals in a hassle-free way while keeping their dietary restrictions in mind. Our website is designed to help you create a personalized meal plan for the week that perfectly suits your lifestyle and dietary preferences.</p>
 
   <p class="lead">
-    <a class="btn btn-primary btn-md border-0" href="#" role="button" style={{ backgroundColor: '#5EC57E'}}>Lets get started!</a>
+    <a class="btn btn-primary btn-md border-0 " href="#" role="button" style={{ backgroundColor: '#5EC57E'}}>Lets get started!</a>
   </p>
   </div>
   <div class="col-sm-6">
       <p class="col-sm-6">
-    HELLO
+      <div className="img-fluid col-md-6 d-flex justify-content-right">
+            <img src={groceries} width="450rem" />
+          </div>
+  
   </p>
   </div>
   </div>
   </div>
 </div>
 
-<p className='mx-5'>
+{/* <p className='mx-5'>
 Our user-friendly interface allows you to choose from a variety of dietary restrictions such as gluten-free, vegan, vegetarian, low-carb, and many more. Once you've selected your preferences, our website will generate a meal plan for the week, complete with recipes that are easy to follow and delicious to taste.
 
 But that's not all, Meal Planner Pro also creates a grocery list based on your meal plan, making your grocery shopping experience a breeze. No more wandering aimlessly around the grocery store trying to remember everything you need, with our website, you'll have a comprehensive list of ingredients at your fingertips.
 
 Our website also allows you to save your favorite recipes and meal plans, making it easy to access them whenever you need them. Whether you're a busy parent, a health enthusiast, or simply someone who wants to make meal planning a breeze, Meal Planner Pro has got you covered.
 
-So why wait? Sign up today and take the first step towards a healthier, happier lifestyle with our personalized meal planning and grocery list services.</p>
+So why wait? Sign up today and take the first step towards a healthier, happier lifestyle with our personalized meal planning and grocery list services.</p> */}
 </div>
   )
   }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import Jumbo from '../components/Jumbotron';
+// import Jumbo from '../components/Jumbotron';
 import MealPlanList from '../components/MealPlanList';
 import Auth from '../utils/auth';
 import capitalizeWords from '../utils/helpers'
 import { Navigate, useParams } from 'react-router-dom';
 import { QUERY_MEALPLAN, QUERY_ME, QUERY_USER } from '../utils/queries';
+import groceries from '../assets/groceries.png'
 
 
 
@@ -30,26 +31,48 @@ console.log(mealPlans)
   return (
   
     <main>
-      {/* jumbotron when signed in */}
-      {Auth.loggedIn(true) ? ((<div class="jumbotron jumbotron-fluid">
+      {/*  when signed in */}
+      {Auth.loggedIn(true) ? ((
+      <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">Hello, {capitalizeWords(user.username)}.</h1>
       <p class="lead"> Welcome to GutGuide. We are here to help you make smart decisions based on your dietary restrictions and weight goals!</p>
     </div>
   </div> )) : 
   // when not signed in
-  (<Jumbo />) }
-      <div className="flex-row justify-center">
-     
-        {/* mealplan list */}
-        <div className="col-12 col-md-8 mb-3">
-        
-<br />
+  (
+  <div  className="jumbo-tron">
+  {/* <div class="container">
+  <div class="row">   </div>
+  <div class="col-sm-6 "> </div>
+  </div> */}
+   <div class="container" style={{ height: '45rem' }}>
+        <div class="row front-page">
+          <div class="col-sm-6 "> 
+          <h1 className="display-2">
+            GutGuide
+          </h1>
+  <p class="d-flex justify-content-center" >The ultimate solution for anyone looking to plan their meals in a hassle-free way while keeping their dietary restrictions in mind. Our website is designed to help you create a personalized meal plan for the week that perfectly suits your lifestyle and dietary preferences.</p>
 
+  <p class="lead">
+    <a class="btn btn-primary btn-md border-0 " href="#" role="button" style={{ backgroundColor: '#5EC57E'}}>Lets get started!</a>
+  </p>
+  </div>
+  <div class="col-sm-6">
+      <p class="col-sm-6">
+      <div className="img-fluid col-md-6 d-flex justify-content-right">
+            <img src={groceries} width="450rem" />
+          </div>
+  
+  </p>
+  </div>
+  </div>
+  </div>
+</div>
+  
 
-        
-        </div>
-      </div>
+  ) }
+    
     </main>
   );
 };
